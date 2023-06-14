7bd0de7e81827778218ee15457826eea76d4ecf8
67ujuj <vauclinjulien@gmail.com>
2023-06-14 14:05:43 +0200

    navbar ok

---
+++ controllers/controller-home.php
@@ -0,0 +1,9 @@
+<?php 
+
+require "../models/data.php";
+
+include "../views/home.php";
+
+function generatePeople($data, $nb = 9){
+    
+}
---
+++ views/components/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navtop navbar navbar-expand-lg bg-body-tertiary">
+  <div class="navbarre container-fluid">
+    <a class="ravage navbar-brand" href="#">ravage</a>
+    <button class="bouton navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="navbarmenu nav-link" href="#">vetements</a>
+        </li>
+        <li class="nav-item">
+          <a class="navbarmenu nav-link" href="#">planches</a>
+        </li>
+        <li class="nav-item">
+          <a class="navbarmenu nav-link" href="#">accessoires</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
