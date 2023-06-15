656ec9879a6eef371e52064ff3d7097bfda721b4
67ujuj <vauclinjulien@gmail.com>
2023-06-15 12:01:53 +0200

    ajout img dans data

--- models/data.php
+++ models/data.php
@@ -44,96 +44,114 @@ $data = [
 			[
 				"brand" => "Jessup",
 				"name" => "Ultra 10\" Grip Skate",
-				"price" => '10.99€'
+				"price" => '10.99€',
+				"img"  => 'grip1.webp'
 			],
 			[
 				"brand" => "Skatedeluxe",
 				"name" => "Chess 9\" Grip Skate",
-				"price" => '7.99€'
+				"price" => '7.99€',
+				"img"  => 'grip2.webp'
 			],
 			[
 				"brand" => "Skatedeluxe",
 				"name" => "Blank 9\" Grip Skate",
-				"price" => '5.99€'
+				"price" => '5.99€',
+				"img"  => 'grip3.webp'
 			],
 			[
 				"brand" => "Grizzly",
 				"name" => "Bear Cut Out Goofy 9\" Grip Skate",
-				"price" => '8.99€'
+				"price" => '8.99€',
+				"img"  => 'grip4.webp'
 			],
 			[
 				"brand" => "Mob Grip",
 				"name" => "x Thrasher Shadow Mag 9\" Grip Skate",
-				"price" => '15.99€'
+				"price" => '15.99€',
+				"img"  => 'grip5.webp'
 			],
 			[
 				"brand" => "Mob Grip",
 				"name" => "x Thrasher Orange 9\" Grip Skate",
-				"price" => '14.99€'
+				"price" => '14.99€',
+				"img"  => 'grip6.webp'
 			]
 		],
 		$roues => [
 			[
 				"brand" => "Spitfire",
 				"name" => "Formula Four Multiswirl Classic Roues 53mm 99A 4 Pack",
-				"price" => '69.99€'
+				"price" => '69.99€',
+				"img"  => 'roues1.webp'
 			],
 			[
 				"brand" => "Spitfire",
 				"name" => "Formula Four Radial Full Roues 56mm 97A 4 Pack",
-				"price" => '69.99€'
+				"price" => '69.99€',
+				"img"  => 'roues2.webp'
 			],
 			[
 				"brand" => "Spitfire",
 				"name" => "Formula Four Multiswirl Classic Roues 54mm 99A 4 Pack",
-				"price" => '69.99€'
+				"price" => '69.99€',
+				"img"  => 'roues3.webp'
 			],
 			[
 				"brand" => "Spitfire",
 				"name" => "x Skate Like A Girl Sessions Formula Four Roues 54mm 99A 4 Pack",
-				"price" => '69.99€'
+				"price" => '69.99€',
+				"img"  => 'roues4.webp'
 			],
 			[
 				"brand" => "Skatedeluxe",
 				"name" => "Academy Club Classic ADV Roues 53mm 100A 4 Pack",
-				"price" => '34.99€'
+				"price" => '34.99€',
+				"img"  => 'roues5.webp'
 			],
 			[
 				"brand" => "Skatedeluxe",
 				"name" => "Barbwore Conical ADV Roues 56mm 100A 4 Pack",
-				"price" => '39.99€'
+				"price" => '39.99€',
+				"img"  => 'roues6.webp'
 			]
 		],
 		$tool => [
 			[
 				"brand" => "Skatedeluxe",
 				"name" => "Multi Outil-Skate",
-				"price" => '24.99€'
+				"price" => '24.99€',
+				"img"  => 'tool1.webp'
 			],
 			[
 				"brand" => "Skatedeluxe",
 				"name" => "Basic Outil-Skate",
 				"price" => '12.99€',
+				"img"  => 'tool2.webp'
 			],
 			[
 				"brand" => "Skatedeluxe",
 				"name" => "Basic Outil-Skate",
-				"price" => '12.99€'
+				"price" => '12.99€',
+				"img"  => 'tool3.webp'
 			],
 			[
 				"brand" => "Silver",
 				"name" => "Basic Outil-Skate",
-				"price" => '31.99€'
+				"price" => '31.99€',
+				"img"  => 'tool4.webp'
 			],
 			[
 				"brand" => "Skatedeluxe",
 				"name" => "Basic Outil-Skate",
-				"price" => '12.99€'
+				"price" => '12.99€',
+				"img"  => 'tool5.webp'
 			],
 			[
 				"brand" => "Skatedeluxe",
 				"name" => "Basic Outil-Skate",
-				"price" => '12.99€'
+				"price" => '12.99€',
+				"img"  => 'tool6.webp'
 			]
 		],
 		$trucks => [
@@ -141,164 +159,192 @@ $data = [
 				"brand" => "Independent",
 				"name" => "144 Stage 11 Bar Standard Truck 8.25\"",
 				"price" => '44.99€',
+				"img"  => 'trucks1.webp'
 			],
 			[
 				"brand" => "Independent",
 				"name" => "139 Mid Reynolds Block Hollow Truck 8\"",
-				"price" => '44.99€'
+				"price" => '44.99€',
+				"img"  => 'trucks2.webp'
 			],
 			[
 				"brand" => "Ace",
 				"name" => "44 AF1 Truck 8.25\"",
-				"price" => '46.99€'
+				"price" => '46.99€',
+				"img"  => 'trucks3.webp'
 			],
 			[
 				"brand" => "Independent",
 				"name" => "144 Stage 11 Standard Delfino Hollow Truck 8.25\"",
-				"price" => '42.99€'
+				"price" => '42.99€',
+				"img"  => 'trucks4.webp'
 			],
 			[
 				"brand" => "Independent",
 				"name" => "x Slayer 149 Stage 11 Standard Truck 8.5\"",
-				"price" => '39.99€'
+				"price" => '39.99€',
+				"img"  => 'trucks5.webp'
 			],
 			[
 				"brand" => "Venture",
 				"name" => "Yuto Great Wave V-Hollow High 5.2\" Truck\"",
-				"price" => '49.99€'
+				"price" => '49.99€',
+				"img"  => 'trucks6.webp'
 			]
 		],
 		$wax => [
 			[
 				"brand" => "Skatedeluxe",
 				"name" => "Square Skatewax",
-				"price" => '5.99€'
+				"price" => '5.99€',
+				"img"  => 'wax1.webp'
 			],
 			[
 				"brand" => "Almost",
 				"name" => "Wac Puck Skatewax",
-				"price" => '7.99€'
+				"price" => '7.99€',
+				"img"  => 'wax2.webp'
 			],
 			[
 				"brand" => "Magenta",
 				"name" => "Stair Grinder Skatewax",
-				"price" => '6.99€'
+				"price" => '6.99€',
+				"img"  => 'wax3.webp'
 			],
 			[
 				"brand" => "Huf",
 				"name" => "Peak Skatewax",
-				"price" => '9.99€'
+				"price" => '9.99€',
+				"img"  => 'wax4.webp'
 			],
 			[
 				"brand" => "Grizzly",
 				"name" => "Grease Skatewax",
-				"price" => '8.99€'
+				"price" => '8.99€',
+				"img"  => 'wax5.webp'
 			],
 			[
 				"brand" => "Primitive",
 				"name" => "x Dragon Ball Super Goku Skatewax",
-				"price" => '11.99€'
+				"price" => '11.99€',
+				"img"  => 'wax6.webp'
 			]
 		]
 	],
 	$chapeaux => [
-		$casquette => [
+		$bob => [
+			[
+				"brand" => "Vans",
+				"name" => "Patch Bucket Chapeau",
+				"price" => '41.99€',
+				"img"  => 'bob1.webp'
+			],
+			[
+				"brand" => "Antix",
+				"name" => "Vaux Bucket Chapeau",
+				"price" => '39.99€',
+				"img"  => 'bob2.webp'
+			],
 			[
 				"brand" => "Carhartt WIP",
-				"name" => "Backley 5 Panel Casquette",
-				"price" => '44.99€'
+				"name" => "Heston Bucket Chapeau",
+				"price" => '64.99€',
+				"img"  => 'bob3.webp'
+			], [
+				"brand" => "Nike SB",
+				"name" => "Sportswear Bucket Chapeau",
+				"price" => '24.99€',
+				"img"  => 'bob4.webp'
 			],
 			[
-				"brand" => "Anuell",
-				"name" => "Moosam 5 Panel Casquette",
-				"price" => '29.99€'
+				"brand" => "Dickies",
+				"name" => "Clarks Grove Bucket Chapeau",
+				"price" => '34.99€',
+				"img"  => 'bob5.webp'
 			],
 			[
-				"brand" => "Anuell",
-				"name" => "Rolam Cord Dad Casquette",
-				"price" => '29.99€'
-			],
-			[
-				"brand" => "Skatedeluxe",
-				"name" => "Can Dad Casquette",
-				"price" => '29.99€'
-			],
-			[
-				"brand" => "Anuell",
-				"name" => "Cavam 5 Panel Casquette",
-				"price" => '29.99€'
-			],
-			[
-				"brand" => "Anuell",
-				"name" => "Rolam Cord Dad Casquette",
-				"price" => '22.99€'
-			],
+				"brand" => "DC",
+				"name" => "x Butter Goods Bucket Chapeau",
+				"price" => '29.99€',
+				"img"  => 'bob6.webp'
+			]
 
 		],
 		$bonnet => [
 			[
 				"brand" => "Anuell",
 				"name" => "Soren Bonnet",
-				"price" => '19.99€'
+				"price" => '19.99€',
+				"img"  => 'bonnet1.webp'
 			],
 			[
 				"brand" => "Skatedeluxe",
 				"name" => "Ufo Bonnet",
-				"price" => '19.99€'
+				"price" => '19.99€',
+				"img"  => 'bonnet2.webp'
 			],
 			[
 				"brand" => "Antix",
 				"name" => "Orto Bonnet",
-				"price" => '19.99€'
+				"price" => '19.99€',
+				"img"  => 'bonnet3.webp'
 			],
 			[
 				"brand" => "Wasted Paris",
 				"name" => "Waffle London Cross Bonnet",
-				"price" => '34.99€'
+				"price" => '34.99€',
+				"img"  => 'bonnet4.webp'
 			],
 			[
 				"brand" => "Anuell",
 				"name" => "Warper Bonnet",
-				"price" => '19.99€'
+				"price" => '19.99€',
+				"img"  => 'bonnet5.webp'
 			],
 			[
 				"brand" => "Nike SB",
 				"name" => "Sportswear Fisherman Bonnet",
-				"price" => '22.99€'
+				"price" => '22.99€',
+				"img"  => 'bonnet6.webp'
 			]
 		],
-		$bob => [
-			[
-				"brand" => "Vans",
-				"name" => "Patch Bucket Chapeau",
-				"price" => '41.99€'
-			],
-			[
-				"brand" => "Antix",
-				"name" => "Vaux Bucket Chapeau",
-				"price" => '39.99€'
-			],
+		$casquette => [
 			[
 				"brand" => "Carhartt WIP",
-				"name" => "Heston Bucket Chapeau",
-				"price" => '64.99€'
-			], [
-				"brand" => "Nike SB",
-				"name" => "Sportswear Bucket Chapeau",
-				"price" => '24.99€'
+				"name" => "Backley 5 Panel Casquette",
+				"price" => '44.99€',
+				"img"  => 'casquette1.webp'
 			],
 			[
-				"brand" => "Dickies",
-				"name" => "Clarks Grove Bucket Chapeau",
-				"price" => '34.99€'
+				"brand" => "Anuell",
+				"name" => "Moosam 5 Panel Casquette",
+				"price" => '29.99€',
+				"img"  => 'casquette2.webp'
 			],
 			[
-				"brand" => "DC",
-				"name" => "x Butter Goods Bucket Chapeau",
-				"price" => '29.99€'
-			]
-
-		]
-
+				"brand" => "Anuell",
+				"name" => "Rolam Cord Dad Casquette",
+				"price" => '29.99€',
+				"img"  => 'casquette3.webp'
+			],
+			[
+				"brand" => "Skatedeluxe",
+				"name" => "Can Dad Casquette",
+				"price" => '29.99€',
+				"img"  => 'casquette4.webp'
+			],
+			[
+				"brand" => "Anuell",
+				"name" => "Cavam 5 Panel Casquette",
+				"price" => '29.99€',
+				"img"  => 'casquette5.webp'
+			],
+			[
+				"brand" => "Anuell",
+				"name" => "Rolam Cord Dad Casquette",
+				"price" => '22.99€',
+				"img"  => 'casquette6.webp'
+			],
+		],
 	]
 ];
